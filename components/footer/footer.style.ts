0aeb2528d1776d23669e96c5d17f2da50444fdda
RomaneeChoiti <tmd1568@gmail.com>
2025-01-23 10:23:33 +0900

    Save local changes before merging

--- components/footer/footer.style.ts
+++ components/footer/footer.style.ts
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 
-const footerStyle = styled.footer``;
-const headerStyle = styled.header``;
+const FooterStyle = styled.footer`
+  background-color: rgb(0, 0, 0);
+  padding: 20px;
+  font-size: 14px;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
 
-export { footerStyle, headerStyle };
+  color: #999;
+
+  p {
+    margin: 0;
+  }
+`;
+const HighlightedText = styled.p`
+  color: rgb(178, 178, 178);
+  font-weight: bold;
+  padding-bottom: 10px;
+  word-spacing: 10px;
+`;
+
+export { FooterStyle, HighlightedText };
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,23 +1,23 @@
+"use client";
+
 import React from "react";
-import { footerStyle, headerStyle } from "./footer.style";
+import { FooterStyle, HighlightedText } from "./footer.style";
 
 const Footer = () => {
   return (
-    <>
-      <div className={footerStyle}>
-        <p>&copy; 2023 A-maewo Club. All rights reserved.</p>
-        <p>
-          고객센터 | 이용약관 | 개인정보처리방침 | 아매워클럽 | 사업자정보확인
-        </p>
-        <p>사업자등록번호 : 123-45-06789</p>
-        <p>통신판매신고번호 : 1111-서울동대문-1111호</p>
-        <p>고객문의 바로가기</p>
-        <p>대표메일 : amaewoclub@gmail.com</p>
-        <p>팀 구성원 : 김주영, 정현수, 최승원, 한정현</p>
-        <p>Copyright © 주식회사 아매워클럽 All right reserved.</p>
-      </div>
-      <div className={headerStyle}></div>
-    </>
+    <FooterStyle>
+      <HighlightedText>고객센터 이용약관 개인정보처리방침</HighlightedText>
+      <p>
+        아매워클럽 | 사업자정보확인 | 사업자등록번호 : 123-45-06789
+        통신판매신고번호 : 1111-서울동대문-1111호
+      </p>
+      <p>
+        고객문의 바로가기 대표메일 : amaewoclub@gmail.com 팀 구성원 : 김주영,
+        정현수, 최승원, 한정현
+      </p>
+      <br />
+      <p>Copyright © 주식회사 아매워클럽 All right reserved.</p>
+    </FooterStyle>
   );
 };
 
