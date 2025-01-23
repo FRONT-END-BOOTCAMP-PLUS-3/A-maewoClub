bbe7be52138c1cbc16b881b44604742b9f03b4bb
Jeonghyun <limithyunii@gmail.com>
2025-01-23 10:26:59 +0900

    [Modify] 01/23 hanjeonghyun 메인 화면 페이지 코드 위치 수정 #7

---
+++ app/(anon)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import SubPage from "@/components/index/pages/subPage";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ScrollContainer = () => {
+  return (
+    <>
+      <SubPage />
+    </>
+  );
+};
+
+export default ScrollContainer;
