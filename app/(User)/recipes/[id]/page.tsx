b27e9faad9d8c2fcef61963fd409d94638b1f749
RomaneeChoiti <tmd1568@gmail.com>
2025-01-24 11:29:08 +0900

    [rename] 01/24 RomanneChoiti 레시피디테일파일구조 #16 - 레시피 디테일 파일 구조 변경 - cookingStep, recipeReview, recipeUserProfile 폴더로 파일 이동 및 이름 변경

--- app/(User)/recipes/[id]/page.tsx
+++ app/(User)/recipes/[id]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Container } from "../recipeDetailPage.style";
-import { RecipeUserProfile } from "../recipeUserProfile";
-import { CookingSteps } from "../cookingSteps";
+import { Container } from "../../../../components/recipe/recipeDetail/recipeDetailPage.style";
+import { RecipeUserProfile } from "../../../../components/recipe/recipeDetail/recipeUserProfile";
+import { CookingSteps } from "../../../../components/recipe/recipeDetail/cookingSteps";
 // import { PhotoReview } from "../photoReview";
 // import { CookReview } from "../cookReview";
 
