45b862a174597150b73f494cec4b3a21d794b95c
hyun9758 <jj020305@likelion.org>
2025-02-03 11:52:59 +0900

    [Feat] 02/03 hyun9758 서치바 컴포넌트 분리 #92 - 중복 코드 수정 - 서치바 컨테이너 삭제

--- components/board/overview/overview.tsx
+++ components/board/overview/overview.tsx
@@ -1,30 +1,15 @@
 "use client";
 
 import React from "react";
-import {
-  SearchInput,
-  SearchIcon,
-  SearchForm,
-} from "../searchBar/searchBar.style";
 import TextForm from "../textForm/textForm";
 import OverviewContainer from "./overview.style";
+import SearchBar from "../searchBar/searchBar";
 
 const Overview = () => {
-  const SearchIconSvg = "/search.svg";
   return (
     <OverviewContainer>
       <TextForm />
-      <SearchForm>
-        {SearchIconSvg && (
-          <SearchIcon
-            src={SearchIconSvg}
-            alt="Search Icon"
-            width={100}
-            height={100}
-          />
-        )}
-        <SearchInput type="text" placeholder="검색..." />
-      </SearchForm>
+      <SearchBar />
     </OverviewContainer>
   );
 };
--- components/board/searchBar/searchBar.tsx
+++ components/board/searchBar/searchBar.tsx
@@ -1,29 +1,22 @@
 "use client";
 
 import React from "react";
-import {
-  SearchBarContainer,
-  SearchInput,
-  SearchIcon,
-  SearchForm,
-} from "./searchBar.style";
+import { SearchInput, SearchIcon, SearchForm } from "./searchBar.style";
 
 const SearchBar = () => {
   const SearchIconSvg = "/search.svg";
   return (
-    <SearchBarContainer>
-      <SearchForm>
-        {SearchIconSvg && (
-          <SearchIcon
-            src={SearchIconSvg}
-            alt="Search Icon"
-            width={100}
-            height={100}
-          />
-        )}
-        <SearchInput type="text" placeholder="검색..." />
-      </SearchForm>
-    </SearchBarContainer>
+    <SearchForm>
+      {SearchIconSvg && (
+        <SearchIcon
+          src={SearchIconSvg}
+          alt="Search Icon"
+          width={100}
+          height={100}
+        />
+      )}
+      <SearchInput type="text" placeholder="검색..." />
+    </SearchForm>
   );
 };
 
