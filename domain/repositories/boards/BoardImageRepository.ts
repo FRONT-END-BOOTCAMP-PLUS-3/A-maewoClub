4529f804fa06a1e763001653bcdfe054d79cde74
hyun9758 <jj020305@likelion.org>
2025-02-03 17:17:34 +0900

    [Feat] 02/03 hyun9758 커뮤니티 어댑터 작성 #118 - 어댑터 GET 부분 작성

--- domain/repositories/boards/BoardImageRepository.ts
+++ domain/repositories/boards/BoardImageRepository.ts
@@ -1,7 +1,6 @@
 import { BoardImage } from "@/domain/entities/boards/BoardImage";
 
-
 export interface BoardImageRepository {
-  findAllByBoardId(BoardId: number): Promise<BoardImage[]>;
-  findDefaultImageByBoardId(BoardId: number): Promise<BoardImage>;
+  findAllByBoardId(boardId: number): Promise<BoardImage[]>;
+  findDefaultImageByBoardId(boardId: number): Promise<BoardImage>;
 }
