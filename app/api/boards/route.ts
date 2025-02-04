b25ccb3a39a57ddbdeddc7879b99cb5a6de9c86c
hyun9758 <jj020305@likelion.org>
2025-02-04 09:44:32 +0900

    [Feat] 02/04 hyun9758 커뮤니티 - Clean_architecture #118 - 파일명 오류 수정

--- app/api/boards/route.ts
+++ app/api/boards/route.ts
@@ -3,7 +3,7 @@ import { BoardRepository } from "@/domain/repositories/boards/BoardRepository";
 import { SbBoardRepository } from "@/infrastructure/repositories/boards/SbBoardRepostory";
 import { BoardImageRepository } from "@/domain/repositories/boards/BoardImageRepository";
 import { SbBoardImageRepository } from "@/infrastructure/repositories/boards/SbBoardImageRepository";
-import { DfBoardsListUsecase } from "@/application/boards/DfBoardListUsecase";
+import { DfBoardListUsecase } from "@/application/boards/DfBoardListUsecase";
 import { BoardListDto } from "@/application/boards/dto/BoardListDto";
 
 // recipes/route
@@ -11,7 +11,7 @@ export async function GET() {
   const boardRepository: BoardRepository = new SbBoardRepository();
   const boardImageRepository: BoardImageRepository =
     new SbBoardImageRepository();
-  const boardImageUsecase: DfBoardsListUsecase = new DfBoardsListUsecase(
+  const boardImageUsecase: DfBoardListUsecase = new DfBoardListUsecase(
     boardRepository,
     boardImageRepository
   );
--- application/boards/DfBoardListUsecase.ts
+++ application/boards/DfBoardListUsecase.ts
@@ -1,4 +1,4 @@
-import { BoardRepository } from "@/domain/repositories/BoardRepository";
+import { BoardRepository } from "@/domain/repositories/boards/BoardRepository";
 import { BoardListDto } from "./dto/BoardListDto";
 import { Board } from "@/domain/entities/boards/Board";
 import { BoardDto } from "./dto/BoardDto";
@@ -6,7 +6,7 @@ import { BoardDto } from "./dto/BoardDto";
 import { BoardImage } from "@/domain/entities/boards/BoardImage";
 import { BoardImageRepository } from "@/domain/repositories/boards/BoardImageRepository";
 
-export class DfBoardsListUsecase {
+export class DfBoardListUsecase {
   constructor(
     private repository: BoardRepository,
     private boardImageRepository: BoardImageRepository
