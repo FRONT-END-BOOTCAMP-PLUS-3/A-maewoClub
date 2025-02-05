6c3140ef37e7266cb8283bd10e9cc9b9c829d779
hyun9758 <jj020305@likelion.org>
2025-02-05 11:23:13 +0900

    [Feat] 02/05 hyun9758 오류 수정 #130 - 오류 중간 수정

--- components/board/main/cardPost/cardPostItem.tsx
+++ components/board/main/cardPost/cardPostItem.tsx
@@ -8,24 +8,10 @@ import {
   PostText,
   PostTitle,
 } from "./cardPost.style";
+import { BoardDto } from "@/application/boards/dto/BoardDto";
 // import { useRouter } from "next/navigation";
 
-interface Post {
-  image: string;
-  title: string;
-  content: string;
-  nickname: string;
-  id: number;
-  comment: number;
-  view: number;
-  heart: number;
-}
-
-interface PostListItemProps {
-  post: Post;
-}
-
-const PostListItem = ({ post }: PostListItemProps) => {
+const PostListItem = (post: BoardDto) => {
   // const router = useRouter();
 
   // const handleDetail = () => {
@@ -36,19 +22,17 @@ const PostListItem = ({ post }: PostListItemProps) => {
     <PostStyle
     // onClick={handleDetail}
     >
-      <PostImage>
-        <img src={post.image} alt={post.title} />
-      </PostImage>
+      <PostImage>{/* <img src={post.image} alt={post.title} /> */}</PostImage>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <PostContent>
           <PostTitle>{post.title}</PostTitle>
-          <PostPhrase>{post.content}</PostPhrase>
-          <PostText>{post.nickname}</PostText>
+          <PostPhrase>{post.description}</PostPhrase>
+          <PostText>{post.userId}</PostText>
         </PostContent>
         <PostText style={{ marginTop: "52px", marginBottom: "11px" }}>
-          <span>댓글: {post.comment}</span>
-          <span>조회수: {post.view}</span>
-          <span>하트: {post.heart} </span>
+          {/* <span>댓글: {post.comment}</span> */}
+          <span>조회수: {post.likeCount}</span>
+          <span>하트: {post.viewCount} </span>
         </PostText>
       </div>
     </PostStyle>
