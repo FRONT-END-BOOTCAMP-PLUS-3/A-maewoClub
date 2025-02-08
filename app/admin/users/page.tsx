072743011150db6c9e3c666824964617f8de4822
Jeonghyun <limithyunii@gmail.com>
2025-02-08 16:29:29 +0900

    [Test] 02/08 hanjeonghyun 임시 데이터 타입 변경

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,118 +1,117 @@
 "use client";
 
 import Table from "@/components/index/admin/table/table";
-
-interface User {
-  id: number;
-  nickname: string;
-  level: number;
-  role: string;
-  email: string;
-  created_at: string;
-  updated_at: string;
-  public_status: string;
-}
+import { User } from "@/domain/entities/User";
 
 const users: User[] = [
   {
-    id: 1,
+    id: "temp_id",
     nickname: "user1",
     level: 2,
     role: "user",
     email: "user1@example.com",
-    created_at: "2024-02-01T12:00:00Z",
-    updated_at: "2024-02-01T15:00:00Z",
-    public_status: "inactive",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 2,
+    id: "temp_id",
+
     nickname: "user2",
     level: 3,
     role: "user",
     email: "user2@example.com",
-    created_at: "2024-02-02T12:00:00Z",
-    updated_at: "2024-02-02T15:00:00Z",
-    public_status: "active",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 3,
+    id: "temp_id",
+
     nickname: "user3",
     level: 4,
     role: "admin",
     email: "user3@example.com",
-    created_at: "2024-02-03T12:00:00Z",
-    updated_at: "2024-02-03T15:00:00Z",
-    public_status: "inactive",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 4,
+    id: "temp_id",
+
     nickname: "user4",
     level: 5,
     role: "user",
     email: "user4@example.com",
-    created_at: "2024-02-04T12:00:00Z",
-    updated_at: "2024-02-04T15:00:00Z",
-    public_status: "active",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 5,
+    id: "temp_id",
+
     nickname: "user5",
     level: 1,
     role: "user",
     email: "user5@example.com",
-    created_at: "2024-02-05T12:00:00Z",
-    updated_at: "2024-02-05T15:00:00Z",
-    public_status: "inactive",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 6,
+    id: "temp_id",
+
     nickname: "user6",
     level: 2,
     role: "admin",
     email: "user6@example.com",
-    created_at: "2024-02-06T12:00:00Z",
-    updated_at: "2024-02-06T15:00:00Z",
-    public_status: "active",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 7,
+    id: "temp_id",
+
     nickname: "user7",
     level: 3,
     role: "user",
     email: "user7@example.com",
-    created_at: "2024-02-07T12:00:00Z",
-    updated_at: "2024-02-07T15:00:00Z",
-    public_status: "inactive",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 8,
+    id: "temp_id",
+
     nickname: "user8",
     level: 4,
     role: "user",
     email: "user8@example.com",
-    created_at: "2024-02-08T12:00:00Z",
-    updated_at: "2024-02-08T15:00:00Z",
-    public_status: "active",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 9,
+    id: "temp_id",
+
     nickname: "user9",
     level: 5,
     role: "admin",
     email: "user9@example.com",
-    created_at: "2024-02-09T12:00:00Z",
-    updated_at: "2024-02-09T15:00:00Z",
-    public_status: "inactive",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
   {
-    id: 10,
+    id: "temp_id",
+
     nickname: "user10",
     level: 1,
     role: "user",
     email: "user10@example.com",
-    created_at: "2024-02-10T12:00:00Z",
-    updated_at: "2024-02-10T15:00:00Z",
-    public_status: "active",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    publicStatus: true,
   },
 ];
 
