708e858adf56a6be0d3ed0db97eb01160323a5c3
RomaneeChoiti <tmd1568@gmail.com>
2025-02-11 11:05:48 +0900

    [refactor] 02/11 RomaneeChoiti reviewModal #173 - 리뷰 모달 refactor

--- app/(user)/recipes/[id]/page.tsx
+++ app/(user)/recipes/[id]/page.tsx
@@ -14,7 +14,7 @@ import {
 import { RecipeUserProfile } from "@/components/recipe/recipeDetail/recipeUserProfile/recipeUserProfile";
 import { PhotoReview } from "@/components/recipe/recipeDetail/recipeReview/photoReview";
 import { CookReview, ReviewData } from "@/components/recipe/recipeDetail/recipeReview/cookReview";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { ReviewModal } from "@/components/recipe/recipeDetail/reviewModal/reviewModal";
 import {
   SortButtonContainer,
@@ -29,41 +29,18 @@ const RecipeDetailPage = () => {
   const [reviewShowAll, setReviewShowAll] = useState(false);
   const [sortType, setSortType] = useState("points");
 
-  // 모달 관련 상태
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedFire, setSelectedFire] = useState<number | null>(null);
-  const [imageName, setImageName] = useState<string | null>(null);
-  const reviewRef = useRef<HTMLTextAreaElement>(null!);
-  const imageRef = useRef<HTMLInputElement>(null!);
-
   const pathname = usePathname(); 
   const pathSegments = pathname.split("/");
   const recipeId = Number(pathSegments[pathSegments.length - 1]);
 
   const [reviewData, setReviewData] = useState<ReviewData[]>([]);
   const [imgData, setImgData] = useState<string[]>([]);
- 
+
+
 
 // TODO : UserId
   const userId = "12546258-59a4-4eb6-86cc-88e2d2421aa1";
-
-  useEffect(() => {
-    const getComments = async (recipeId: number) => {
-      try {
-        const res = await fetch(`/api/recipe-comments?recipeId=${recipeId}`,
-          {
-            method: "GET",
-          });
-        const data = await res.json();  
-        setReviewData(data);
-        setImgData(data.map((review: ReviewData) => review.imageUrl));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getComments(recipeId);
-    }, [recipeId]);
-
 
   const handleReview = () => {
     setReviewShowAll(true);
@@ -81,25 +58,9 @@ const RecipeDetailPage = () => {
   };
 
   const handleCloseModal = () => {
-    setSelectedFire(null);
-    setImageName(null);
     setIsModalOpen(false);
   };
 
-  const handleFireClick = (index: number) => {
-    setSelectedFire(index);
-  };
-  
-
-  // 이미지 파일 이름 표시
-  const handleImageChange = () => {
-    const image = imageRef.current?.files?.[0];
-    if (image) {
-      setImageName(image.name);
-    }
-  };
-
-  // 레시피 상세페이지에서 보여줄 데이터
   const stepsToShow = showAllSteps ? testDatas : testDatas.slice(0, 2);
 
 
@@ -118,12 +79,6 @@ const RecipeDetailPage = () => {
       <ReviewModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
-        selectedFire={selectedFire}
-        handleFireClick={handleFireClick}
-        reviewRef={reviewRef}
-        imageRef={imageRef}
-        handleImageChange={handleImageChange}
-        imageName={imageName}
         userId={userId}
         recipeId={recipeId}
         isUpdate={false}
