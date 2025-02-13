e665dc815c0d241442e826b161b7888b68db5350
Jeonghyun <limithyunii@gmail.com>
2025-02-13 09:35:07 +0900

    [Refactor] 02/13 hanjeonghyun 디테일 이슈 해결 #215

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -15,7 +15,7 @@ import { useAuthStore } from "@/store/useAuthStore";
 import { UserInfoModal } from "../user/login/userInfoModal/userInfoModal";
 import Image from "next/image";
 
-const logo = '/logo.png';
+const logo = "/logo.png";
 
 export const Header = () => {
   const { user, isAuthenticated, logout } = useAuthStore();
@@ -24,52 +24,49 @@ export const Header = () => {
   const closeLoginModal = () => setShowLogin(false);
 
   const [showUserInfoModal, setShowUserInfoModal] = useState(false);
-  const toggleUserInfoModal = () =>{
-     setShowUserInfoModal((prev) => !prev)
-    }
+  const toggleUserInfoModal = () => {
+    setShowUserInfoModal((prev) => !prev);
+  };
   const closeUserInfoModal = () => setShowUserInfoModal(false);
-
 
   return (
     <>
       <HeaderStyle>
         <LogoStyle>
           <LinkStyle href='/'>
-          <Image 
-            src={logo}
-            alt='logo'
-            height={150}
-            width={150}
-          />
+            <Image
+              src={logo}
+              alt='logo'
+              height={150}
+              width={150}
+            />
           </LinkStyle>
         </LogoStyle>
         <NavStyle>
           <LinkStyle href='/recipes'>레시피</LinkStyle>
           <LinkStyle href='/boards'>커뮤니티</LinkStyle>
           <LinkStyle href='/quests'>챌린지</LinkStyle>
-          <LinkStyle href='/admin'>관리자</LinkStyle>
         </NavStyle>
         <LoginStyle>
           {isAuthenticated ? (
             <ProfileWrapper onClick={toggleUserInfoModal}>
               <img
                 src={user?.photoUrl}
-                alt=''
+                alt='profile photo'
               />
             </ProfileWrapper>
           ) : (
             <LoginBtnStyle
-            href='#'
-            onClick={(e) => {
-              e.preventDefault();
-              openLoginModal();
-            }}
+              href='#'
+              onClick={(e) => {
+                e.preventDefault();
+                openLoginModal();
+              }}
             >
               Login
             </LoginBtnStyle>
           )}
         </LoginStyle>
-
       </HeaderStyle>
 
       {showLogin && <Login onClose={closeLoginModal} />}
